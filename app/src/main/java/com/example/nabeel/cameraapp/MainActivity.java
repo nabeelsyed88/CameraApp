b9937a013d29df4c920ebdf153bed75860f8996f
Nabeel Syed <nabeelsyed88@yahoo.com>
2018-07-27 10:23:09 -0400

    Added Start Pause Buttons

--- app/src/main/java/com/example/nabeel/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/nabeel/cameraapp/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.nabeel.cameraapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
     VideoView videoView;
+    boolean videoTaken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         videoView = findViewById(R.id.vv_main);
+
+        videoTaken = false;
     }
 
     private void dispatchTakeVideoIntent() {
@@ -33,11 +38,29 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
             Uri videoUri = data.getData();
             videoView.setVideoURI(videoUri);
-            videoView.start();
+            videoTaken = true;
         }
     }
 
     public void takeVideo(View view) {
         dispatchTakeVideoIntent();
+    }
+
+    public void pauseVideo(View view) {
+        if(videoTaken) {
+            if(videoView.isPlaying())
+                videoView.pause();
+        } else {
+            Toast.makeText(this, "Video not taken yet", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void playVideo(View view) {
+        if(videoTaken) {
+            if(!videoView.isPlaying())
+                videoView.start();
+        } else {
+            Toast.makeText(this, "Video not taken yet", Toast.LENGTH_SHORT).show();
+        }
     }
 }
